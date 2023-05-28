fb4ce35bc38f3a43adffaefed46b2243d58f73d8
Teddy55Codes <teddy@teddy.email>
2023-05-28 21:23:39 +0200

    settings and varibles are now at the start of combined.ow

--- combine.py
+++ combine.py
@@ -2,11 +2,23 @@ import os
 
 ow_directory = "WorkshopMode"
 output_file = "combined.ow"
+header_files = ["Settings.ow", "Variables.ow"]
 contents = []
+
+def check_excluded(filename):
+    for file in header_files:
+        if filename == file:
+            return True
+    return False
+
+for file in header_files:
+    with open(os.path.join(ow_directory, file), 'r') as f:
+                contents.append(f.read() + "\n")
+
 
 for root, dirs, files in os.walk(ow_directory):
     for file in files:
-        if file.endswith(".ow"):
+        if file.endswith(".ow") and not check_excluded(file):
             with open(os.path.join(root, file), 'r') as f:
                 contents.append(f.read() + "\n")
 
